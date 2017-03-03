c955ea1d57bcfd76dda5b2aa7893811c88f6328d
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-03 10:09:32 -0600

    Added Start/Restart buttons in HTML Cleaned up JS with Comments Created a function to add new random colors to an array

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -1,7 +1,30 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 "use strict";
+var colors = ["red", "yellow", "green", "blue"];
+var computerSequence = [];
+var iterator = 0;
+var userInput = [];
+var start = "off";
 
+//====================================================
+//		Start/Restart Button, need to connect to game
+//====================================================
+$("#start").click(function(){
+	computerSequence == [];
+	userInput ==[];
+	$("#start").hide();
+	$("#restart").show();
+});
+$("#restart").click(function(){
+	computerSequence == [];
+	userInput ==[];
+	$("#restart").hide();
+	$("#start").show();
+});
 
+//====================================================
+//		User click Function to light up squares
+//====================================================
 $(".square").click(function(click){
 	animateClick($(this));
 });
@@ -15,18 +38,37 @@ function animateClick(element) {
 	}, 100);
 }
 
-var colors = ["red", "yellow", "green", "blue"];
-
+//====================================================
+//		How to generate random colors
+//====================================================
 function getRandomColor(){
 	var randomNumber = Math.floor(Math.random()* 4);
-	console.log(colors[randomNumber]);
+	return colors[randomNumber];
 }
 
+//====================================================
+//		This adds getRandomColor to end of computerSequence array
+//====================================================
+function addNewSquare() {
+	computerSequence.push(getRandomColor());
+	return computerSequence;
+}
+
+//====================================================
+//	This doesn't work, but supposed to light up squares
+//====================================================
 function animateRandomSquare() {
-	var squareToAnimate = getRandomColor();
+	var squareToAnimate = computerSequence[iterator];
 	animate(squareToAnimate);
+	$(element).removeClass("down");
+	$(element).addClass("up");
+	setTimeout(function(){
+		$(element).removeClass("up");
+		$(element).addClass("down");
+	}, 1000);
+
 }
 
 getRandomColor();
 
-});
+// });
